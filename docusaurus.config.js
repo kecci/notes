1b85bedc5d7db1002edefa95d4ddbb68cb49b720
kecci <abyanjksatu@gmail.com>
2022-10-15 00:46:22 +0700

    update temporary

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
         },
         items: [
           {
-            to: "/publication",
+            to: "/blog",
             label: "Publication",
             position: "right"
           },
